61c6cee5335ee82f2505244cb358bca4f30160d0
atsushimemet <noap3b69n@gmail.com>
2023-05-14 17:31:46 +0900

    Update ホストの追加

--- fight_calendar_x/fight_calendar_x/settings.py
+++ fight_calendar_x/fight_calendar_x/settings.py
@@ -25,7 +25,7 @@ from .local_settings import SECRET_KEY  # noqa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
