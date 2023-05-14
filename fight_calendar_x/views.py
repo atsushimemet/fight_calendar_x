95da7188f1e28d9bd2ac6a335745bed410d56dbf
atsushimemet <noap3b69n@gmail.com>
2023-05-14 18:08:37 +0900

    Update viewsの変更

--- fight_calendar_x/views.py
+++ fight_calendar_x/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h1>Hello</h1>")
+    return render(request, "fight_calendar_x/index.html")
